d6bab8b8040b97cc6ff2b4c5ae4777c8bd04f80c
l630003017 <l630003017@UTC.local>
2019-04-16 15:55:02 +0800

    ingredient class and recipe ingredient class

---
+++ BrewDay/BrewDay/src/Ingredient.java
@@ -0,0 +1,55 @@
+
+public class Ingredient {
+
+	private String ID;
+	private int value;
+	private String name;
+	private String unit;
+
+	Ingredient(String name, String unit) {
+		this.name = name;
+		this.unit = unit;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	public static void main (String[] args) {
+		Ingredient ingre1 = new Ingredient("pijiuhua", "g"); 
+		ingre1.setValue(20);
+		System.out.println(ingre1.getValue());
+		System.out.println(ingre1.getName());
+	}
+
+}
+
+
---
+++ BrewDay/BrewDay/src/StorageIngredient.java
@@ -0,0 +1,9 @@
+
+public class StorageIngredient extends Ingredient {
+
+	StorageIngredient(String name, String unit) {
+		super(name, unit);
+		// TODO Auto-generated constructor stub
+	}
+
+}
