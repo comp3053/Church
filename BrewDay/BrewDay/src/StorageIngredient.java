ad8cc39cd6868e9d9b3d28b8cfed0cebbdcaa15a
Ruonan Chen <chennylan@163.com>
2019-04-18 16:54:20 +0800

    modify the Brew Day project

--- BrewDay/BrewDay/src/StorageIngredient.java
+++
@@ -1,9 +0,0 @@
-
-public class StorageIngredient extends Ingredient {
-
-	StorageIngredient(String name, String unit) {
-		super(name, unit);
-		// TODO Auto-generated constructor stub
-	}
-
-}
---
+++ BrewDay/src/Model/StorageIngredient.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class StorageIngredient extends Ingredient {
+
+	StorageIngredient(String name, String unit) {
+		super(name, unit);
+		// TODO Auto-generated constructor stub
+	}
+
+}
