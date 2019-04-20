25682fc56829fc5cabc8c8a3140238703df05601
chenlanlanlan <chennylan@163.com>
2019-04-20 19:20:13 +0800

    Upload recipeingredient file

---
+++ BrewDay/src/Model/RecipeIngredient.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class RecipeIngredient extends Ingredient {
+	
+	public RecipeIngredient(String name, String unit) {
+		// TODO Auto-generated constructor stub
+		super(name, unit);
+	}
+	
+	public boolean updateAmount(String name, int amount) {
+		return true;
+	}
+	
+	
+
+}
