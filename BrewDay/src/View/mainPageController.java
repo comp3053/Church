9173242d7b442db4a0995965ec8eaa8b6c6aa201
Nina <l630003038@mail.uic.edu.hk>
2019-05-02 22:08:00 +0800

    I am crazy now! I want to go home!

---
+++ BrewDay/src/View/mainPageController.java
@@ -0,0 +1,20 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public abstract class mainPageController implements Initializable{
+	
+	@FXML
+	private void jump(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	private void jump_recipe(ActionEvent event) {
+		
+	}
+	
+	
+}
