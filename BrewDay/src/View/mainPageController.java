69743dfa399eb3bef1dd8318013beaa6a5b0f65b
Nina <l630003038@mail.uic.edu.hk>
2019-05-03 23:09:05 +0800

    Main page controller

--- BrewDay/src/View/mainPageController.java
+++ BrewDay/src/View/mainPageController.java
@@ -1,20 +1,28 @@
 package View;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 public abstract class mainPageController implements Initializable{
 	
-	@FXML
-	private void jump(ActionEvent event) {
+	//@FXML
+	//private void jump(ActionEvent event) {
+		//Start.getInstance().jump();
+	//}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		// TODO Auto-generated method stub
 		
 	}
-	
-	@FXML
-	private void jump_recipe(ActionEvent event) {
-		
-	}
-	
-	
 }
