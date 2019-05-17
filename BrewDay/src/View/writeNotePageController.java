c5aa093c1c0359cfadd412597e6de73c4ea57ef7
Jerrykuo0310 <jsjdand@163.com>
2019-05-17 17:03:03 +0800

    write note function

---
+++ BrewDay/src/View/writeNotePageController.java
@@ -0,0 +1,41 @@
+package View;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Model.Database;
+import Model.Ingredient;
+import Model.StorageIngredient;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class writeNotePageController implements Initializable{
+	
+	@FXML
+	private TextField recipeName;
+	@FXML
+	private TextField brewID;
+	@FXML
+	private TextField date;
+	
+	
+	@FXML
+	public void backToMaintainIngredient(ActionEvent event) {
+		Start.getInstance().maintainIngredient();
+	}
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
