4fcf3cdcce498be81e5f449714ec3a410b4e455f
Jerrykuo0310 <jsjdand@163.com>
2019-05-19 06:07:34 +0800

    adjust UI

--- BrewDay/src/View/UpdateRecipeController.java
+++ BrewDay/src/View/UpdateRecipeController.java
@@ -3,11 +3,40 @@ package View;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Model.StorageIngredient;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class UpdateRecipeController implements Initializable{
+	
+	//for show data
+		ObservableList<StorageIngredient> StorageIngredientList = FXCollections.observableArrayList();
+		@FXML
+		private TableView<StorageIngredient> ingredientTable = new TableView<StorageIngredient>(StorageIngredientList);
+		@FXML
+		private TableColumn<StorageIngredient, String> nameList;
+		@FXML
+		private TableColumn<StorageIngredient, Integer> stockList;
+		@FXML
+		private TableColumn<StorageIngredient, String> unitList;
+		
+		
+		@FXML
+		private TextField inputValue;
+		
+	
+	
+	
+	
+	
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) 
 	{
