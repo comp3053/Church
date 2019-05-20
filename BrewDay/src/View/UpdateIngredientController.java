a230427ec21c21a38d8584c2c5693b4d01ed828c
ltx0043 <2722167741@qq.com>
2019-05-20 15:09:33 +0800

    modify update ingredient alerts

--- BrewDay/src/View/UpdateIngredientController.java
+++ BrewDay/src/View/UpdateIngredientController.java
@@ -27,7 +27,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 public class UpdateIngredientController implements Initializable{
-	
+
 	//for show data
 	ObservableList<StorageIngredient> StorageIngredientList = FXCollections.observableArrayList();
 	@FXML
@@ -38,51 +38,82 @@ public class UpdateIngredientController implements Initializable{
 	private TableColumn<StorageIngredient, Integer> stockList;
 	@FXML
 	private TableColumn<StorageIngredient, String> unitList;
-	
-	
+
+
 	@FXML
 	private TextField inputValue;
-	
-	
+
+
 	@FXML
 	public void addStock(ActionEvent event) {
 		StorageIngredient storageIngredient = ingredientTable.getSelectionModel().getSelectedItem();
+		if(storageIngredient == null) {
+			Start.getInstance().warningMsg("Invalid input", "Please select a ingredient to update!");
+			return;
+		}
+		int stock = 0;
 		try{
-			int stock = Integer.parseInt(inputValue.getText());
+			stock = Integer.parseInt(inputValue.getText());
 			updateIngredient(storageIngredient, 1, stock);
 		}catch (NumberFormatException e) {
 			// TODO: handle exception
-			Start.getInstance().warningMsg("Invalid input", "The value should be integer(s)");
+			if(stock == 0) {
+				Start.getInstance().warningMsg("Invalid input", "Please input the update information");
+				Start.getInstance().updateIngredient();
+				return;
+			}
+			Start.getInstance().warningMsg("Invalid input", "The value should be positive integer(s)");
 			Start.getInstance().updateIngredient();
 			return;
 		}
-		
+
 	}
-	
+
 	@FXML
 	public void subtractStock(ActionEvent event) {
 		StorageIngredient storageIngredient = ingredientTable.getSelectionModel().getSelectedItem();
-		int stock = Integer.parseInt(inputValue.getText());
-		
-		if (stock > storageIngredient.getStock()) {
-			Alert alert = new Alert(AlertType.WARNING);
-			alert.setTitle("Warning");
-			alert.setHeaderText("Out of stock");
-			alert.setContentText("The current stock is unavailable for the operation!");
-			ButtonType buttonTypeOK = new ButtonType("Back");
-			alert.getButtonTypes().setAll(buttonTypeOK);
-			alert.showAndWait();
+		if(storageIngredient == null) {
+			Start.getInstance().warningMsg("Invalid input", "Please select a ingredient to update!");
+			return;
 		}
-		else {
-			updateIngredient(storageIngredient, 0, stock); //subtract
+		int stock= 0;
+		try {
+			stock = Integer.parseInt(inputValue.getText());
+
+			if (stock > storageIngredient.getStock()) {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Warning");
+				alert.setHeaderText("Out of stock");
+				alert.setContentText("The current stock is unavailable for the operation!");
+				ButtonType buttonTypeOK = new ButtonType("Back");
+				alert.getButtonTypes().setAll(buttonTypeOK);
+				alert.showAndWait();
+			}
+			else {
+				updateIngredient(storageIngredient, 0, stock); //subtract
+			}
+		}catch (NumberFormatException e) {
+			// TODO: handle exception
+			if(stock == 0) {
+				Start.getInstance().warningMsg("Invalid input", "Please input the update information");
+				Start.getInstance().updateIngredient();
+				return;
+			}
+			Start.getInstance().warningMsg("Invalid input", "The value should be positive integer(s)");
+			Start.getInstance().updateIngredient();
+			return;
 		}
-		
 	}
-	
+
 	//choice 0:  subtract 
 	//choice 1:  add
 	public void updateIngredient(StorageIngredient ingredient, int choice, int stock) {
 		int tempStock = ingredient.getStock();
+		if(stock<0) {
+			Start.getInstance().warningMsg("Invalid input", "The value should be positive integer(s)");
+			return;
+		}
+		//subtract ingredient stock
 		if (choice == 0) {
 			if(tempStock - stock == 0) {
 				ingredient.setStock(0);
@@ -94,37 +125,37 @@ public class UpdateIngredientController implements Initializable{
 		else if (choice == 1) {
 			ingredient.setStock(tempStock + stock);
 		}
-		
+
 		Database database = new Database();
 		database.updateStorgeIngredient(ingredient);
-		
+
 		Start.getInstance().confirmMsg("Success!", "The Ingredient has been updated!");
 		Start.getInstance().updateIngredient();
 	}
-	
+
 	@FXML
 	public void backToMaintainIngredient(ActionEvent event) {
 		Start.getInstance().maintainIngredient();
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) 
 	{
 		// TODO Auto-generated method stub
 		StorageIngredientList.clear();
-		
+
 		ArrayList<StorageIngredient> ingredientList = new ArrayList<StorageIngredient>();
 		Database db = new Database();
 		ingredientList = db.getStorgeIngredientList();
-		
+
 		for(StorageIngredient ingredient:ingredientList) {
 			StorageIngredientList.add(ingredient);
 		}
-		
+
 		nameList.setCellValueFactory(new PropertyValueFactory<StorageIngredient, String>("Name"));
 		stockList.setCellValueFactory(new PropertyValueFactory<StorageIngredient, Integer>("Stock"));
 		unitList.setCellValueFactory(new PropertyValueFactory<StorageIngredient, String>("Unit"));
-		
+
 		ingredientTable.setItems(StorageIngredientList);
 	}
 
